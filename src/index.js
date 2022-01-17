ab304516661be1ad5e914b40801a31b55c1fb041
pallav verma <pallavverma@pallavs-MacBook-Air.local>
2022-01-17 10:36:37 +0530

    this is a test

---
+++ src/index.js
@@ -0,0 +1,10 @@
+import React from "react"
+import ReactDom from "react-dom";
+
+
+ReactDom.render(
+  <div>
+    <h1 className="heading">Pallav</h1>
+  </div>,
+  document.getElementById("root")
+)
